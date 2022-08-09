cd2f369e038eb06116cc31440bc7ab73ee6b1f6b
Bildushkina Tuyana <Budaeva.tuyana.garmaevna@gmail.com>
2022-08-09 15:44:43 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(number1);
+    Console.Write("min = ");
+    Console.WriteLine(number2);
+}
+else if (number2 > number1)
+{
+    Console.Write("max = ");
+    Console.WriteLine(number2);
+    Console.Write("min = ");
+    Console.WriteLine(number1);
+}
