23fea8e53034ea903299983580a75ecd26b23560
Bildushkina Tuyana <Budaeva.tuyana.garmaevna@gmail.com>
2022-08-10 10:21:02 +0300

    Task 3 changes

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if(number%2 == 0)
+if (number % 2 == 0)
 {
     Console.WriteLine("Четное");
 }
 else
 {
-  Console.WriteLine("Нечетное");
+    Console.WriteLine("Нечетное");
 }
