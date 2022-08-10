c8855ddb064a4fedd7fb2b7ded3273d6952783aa
Bildushkina Tuyana <Budaeva.tuyana.garmaevna@gmail.com>
2022-08-10 17:53:14 +0300

    Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (i == 1)
+{
+    i = i + 1;
+}
+
+while (i <= number)
+{
+    Console.WriteLine(i);
+    i = i + 2;
+}
